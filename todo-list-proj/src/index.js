a6386f9aead237472ea2f1997e1e5f0442a22d03
mattpettenato <mattpettenato@gmail.com>
2022-09-23 17:26:40 -0700

    edit working

--- todo-list-proj/src/index.js
+++ todo-list-proj/src/index.js
@@ -1,3 +1,27 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+// const addTodo = document.getElementById('add-todo');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const prePayload = new FormData(form);
+  const payload = new URLSearchParams(prePayload)
+
+  // console.log([...payload])
+
+  fetch('http://localhost:3000/todos', {
+    method: 'POST',
+    body: payload
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+    window.location.reload();
+});
+
+
+
 const app = document.getElementById('root')
 
 const container = document.createElement('div')
@@ -8,8 +32,8 @@ app.appendChild(container)
 var request = new XMLHttpRequest()
 
 request.open('GET', 'http://localhost:3000/todos', true)
+
 request.onload = function () {
-  // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
     data.forEach(todos => {
@@ -20,44 +44,77 @@ request.onload = function () {
       h1.textContent = todos.title
 
       const p = document.createElement('p')
-      
-      p.textContent = `${todos.completed}...`
+      if (todos.completed === true) {
+        p.textContent = 'Completed'
+      } else {
+        p.textContent = 'Not Completed'
+      }
 
       container.appendChild(item)
       item.appendChild(h1)
       item.appendChild(p)
 
-const someData = {
-  // title: document.querySelector(todos.title).value
-  title: "edited",
-  completed: true
-}
-
-
       const editButton = document.createElement('button')
-      editButton.innerText = 'Edit Task'
-      editButton.id = 'editTaskButton'
+      editButton.textContent = 'Edit'
+      editButton.id= 'editButton'
       editButton.addEventListener('click', () => {
-        let url = `http://localhost:3000/todos/${todos.id}`
-        let options = {
-          method: "PUT",
-          title: JSON.stringify(someData)
-        }
-      fetch(`http://localhost:3000/todos/${todos.id}`, {
-      method: `PATCH`,
-      body: JSON.stringify({
-        title: "edited 223",
-        completed: false
-      }),
-      headers: { 'Content-type': `application/json; charset=UTF-8` },
-    }).then((response) => response.json())
-    window.location.reload();
+        const editForm = document.createElement('form')
+        editForm.id = 'editForm'
+        editForm.method = 'PUT'
+        editForm.action = 'http://localhost:3000/todos/' + todos.id
+
+        const editInput = document.createElement('input')
+        editInput.id = 'editInput'
+        editInput.name = 'title'
+        editInput.type = 'text'
+        editInput.value = todos.title
+
+        const editSubmit = document.createElement('input')
+        editSubmit.id = 'editSubmit'
+        editSubmit.type = 'submit'
+        editSubmit.value = 'Submit'
+
+        editForm.appendChild(editInput)
+        editForm.appendChild(editSubmit)
+        item.appendChild(editForm)
+
+        editForm.addEventListener('submit', (e) => {
+          e.preventDefault();
+
+          const prePayload = new FormData(editForm);
+          const payload = new URLSearchParams(prePayload)
+
+          // console.log([...payload])
+
+          fetch('http://localhost:3000/todos/' + todos.id, {
+            method: 'PATCH',
+            body: payload
+          })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+            window.location.reload();
+        })
       })
+    
+
+      // const editButton = document.createElement('button')
+      // editButton.innerText = 'Edit'
+      // editButton.id = 'mainButton'
+      //   editButton.addEventListener('click', () => {
+      //   let url = `http://localhost:3000/todos/${todos.id}`
+      //   let options = {
+      //     method: "PATCH"
+      //   }
+      //   fetch(url, options)
+      //   .then(response => console.log(response.status))
+      //   window.location.reload();
+      // })
       item.appendChild(editButton)
 
 
       const button = document.createElement('button')
-      button.innerText = 'Delete To-Do?'
+      button.innerText = 'Delete'
       button.id = 'mainButton'
         button.addEventListener('click', () => {
         let url = `http://localhost:3000/todos/${todos.id}`
